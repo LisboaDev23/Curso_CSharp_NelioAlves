ba8a44f6225f203601b6da42bb4924d5b16bef79
PMENOS.COM.BR\139490 <gabriellisboa@pmenos.com.br>
2024-12-05 11:55:27 -0300

    Primeira atividade concluída

--- Atividades/Recapitulacao-Logica/Atividade1/Atividade1/Program.cs
+++ Atividades/Recapitulacao-Logica/Atividade1/Atividade1/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+
+public class Program
+{
+    public static void Main (string[] args)
+    {
+        string produto = Console.ReadLine();
+        string produto2 = Console.ReadLine();
+
+        byte idade = byte.Parse(Console.ReadLine());
+        int codigo = int.Parse(Console.ReadLine());
+        char genero = char.Parse(Console.ReadLine());
+
+        double preco = double.Parse(Console.ReadLine());
+        double preco2 = double.Parse(Console.ReadLine());
+        double medida = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Produtos:");
+        Console.WriteLine($"{produto}, cujo preço é {preco}");
+        Console.WriteLine($"{produto2}, cujo preço é {preco2}");
+
+        Console.WriteLine($"Registro: {idade} anos de idade, código {5290} e gênero: {genero}");
+
+        Console.WriteLine($"Medida com 8 casas decimais: {medida}");
+        Console.WriteLine($"Arredondado (3 casas decimais): {medida.ToString("F3")}");
+        Console.WriteLine($"Separador decimal invariant culture: {medida.ToString("F3",CultureInfo.InvariantCulture)}");
+    }
+}
