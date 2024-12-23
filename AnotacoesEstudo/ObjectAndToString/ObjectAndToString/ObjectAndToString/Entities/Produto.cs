4802173c072f436a922e21a17c364b8b182d4306
Gabriel Pereira Lisboa <gabriel.lisboadv@gmail.com>
2024-12-23 19:08:13 -0300

    Exercicio exemplo para entender classe Object e sobre o método ToString(), entendendo também sobre a sobreescrita de métodos com C#

---
+++ AnotacoesEstudo/ObjectAndToString/ObjectAndToString/ObjectAndToString/Entities/Produto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectAndToString.Entities
+{
+    public class Produto
+    {
+        public Produto() { }
+
+        public Produto(string Nome, double Preco, int QtdEstoque)
+        {
+            this.Nome = Nome;
+            this.Preco = Preco;
+            this.QtdEstoque = QtdEstoque;
+        }
+
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public int QtdEstoque { get; set; }
+
+
+        //sobrescrita de método, somente colocar primeiramente o override e o vs irá dar o autocomplete
+        public override string ToString()
+        {
+            return Nome + ", $" + Preco + ", " + QtdEstoque;
+        }
+    }
+}
---
+++ AnotacoesEstudo/ObjectAndToString/ObjectAndToString/ObjectAndToString/Program.cs
@@ -0,0 +1,13 @@
+﻿using ObjectAndToString.Entities;
+
+public class Program
+{
+
+    public static void Main (string[] args)
+    {
+        Produto produto = new Produto("Paracetamol", 20.00, 200);
+
+        Console.WriteLine(produto);
+
+    }
+}
