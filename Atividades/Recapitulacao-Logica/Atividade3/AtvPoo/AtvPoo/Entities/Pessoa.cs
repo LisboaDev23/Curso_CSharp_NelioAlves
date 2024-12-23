b5d5be562623efef45e79a9294f472b69a4fb9b1
Gabriel Pereira Lisboa <gabriel.lisboadv@gmail.com>
2024-12-23 18:24:49 -0300

    Atividade POO Concluída

---
+++ Atividades/Recapitulacao-Logica/Atividade3/AtvPoo/AtvPoo/Entities/Pessoa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtvPoo.Entities
+{
+    public class Pessoa
+    {
+        public Pessoa() { }
+        public Pessoa(string Nome, int Idade, char Sexo)
+        {
+            this.Nome = Nome;
+            this.Idade = Idade;
+            this.Sexo = Sexo;
+        }
+        public string Nome {  get; set; }
+        public int Idade { get; set; }
+        public char Sexo { get; set; }
+    }
+}
---
+++ Atividades/Recapitulacao-Logica/Atividade3/AtvPoo/AtvPoo/Program.cs
@@ -0,0 +1,29 @@
+﻿using AtvPoo.Entities;
+
+public class Program
+{
+    public static void Main (string[] args)
+    {
+        Pessoa p1, p2;
+
+        p1 = new Pessoa();
+        p2 = new Pessoa();
+
+        Console.WriteLine("Digite o nome da pessoa 1");
+        p1.Nome = Console.ReadLine();
+        Console.WriteLine("Digite a idade da pessoa 1");
+        p1.Idade = int.Parse(Console.ReadLine());
+        Console.WriteLine("Digite o sexo da pessoa 1 em M ou F");
+        p1.Sexo = char.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o nome da pessoa 2");
+        p2.Nome = Console.ReadLine();
+        Console.WriteLine("Digite a idade da pessoa 2");
+        p2.Idade = int.Parse(Console.ReadLine());
+        Console.WriteLine("Digite o sexo da pessoa 2 em M ou F");
+        p2.Sexo = char.Parse(Console.ReadLine());
+
+        Console.WriteLine(p1.Nome + " " + p1.Idade + " " + p1.Sexo);
+        Console.WriteLine(p2.Nome + " " + p2.Idade + " " + p2.Sexo);
+    }
+}
