4b6da8a5f386105c7f664d63e14b2bd3845ad090
Gabriel Pereira Lisboa <gabriel.lisboadv@gmail.com>
2025-05-26 20:48:13 -0300

    Atividade Retângulo

---
+++ Atividades/Classes, métodos/Atividade1/Atividade1/Program.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Atividade1
+{
+    public class Program
+    {
+        public static void Main (string[] args)
+        {
+            //capturar a altura 
+            Console.WriteLine("Digite a altura do retângulo:");
+            double altura = double.Parse(Console.ReadLine());
+            //capturar a largura
+            Console.WriteLine("Digite a largura do retângulo");
+            double largura = double.Parse(Console.ReadLine());
+
+            Retangulo retangulo = new Retangulo(altura, largura);
+
+            Console.WriteLine("Área: " + retangulo.Area());
+            Console.WriteLine("Perimetro: " + retangulo.Perimetro());
+            Console.WriteLine("Diagonal: " + retangulo.Diagonal());
+        }
+    }
+}
---
+++ Atividades/Classes, métodos/Atividade1/Atividade1/Retangulo.cs
@@ -0,0 +1,34 @@
+﻿namespace Atividade1
+{
+    public class Retangulo
+    {
+
+        public double Largura { get; set; }
+
+        public double Altura { get; set; }
+
+        public Retangulo (double altura, double largura)
+        {
+            this.Altura = altura;
+            this.Largura = largura;
+        }
+
+        public double Area ()
+        {
+            return Largura * Altura;
+        }
+
+        public double Perimetro ()
+        {
+            return 2 * (Largura + Altura);
+        }
+
+        public double Diagonal ()
+        {
+            double potenciaLargura = Math.Pow (Largura, 2);
+            double potenciaAltura = Math.Pow (Altura, 2);
+
+            return Math.Sqrt(potenciaAltura + potenciaLargura);
+        }
+    }
+}
