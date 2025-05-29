158f2e1bb9743376cc6b8d0c3ef9f205fdef96dd
Gabriel Pereira Lisboa <gabriel.lisboadv@gmail.com>
2025-05-29 18:58:19 -0300

    Atividade 2 concluída

---
+++ Atividades/Classes, métodos/Atividade 2/Atividade2/Atividade2/Funcionario.cs
@@ -0,0 +1,29 @@
+﻿namespace Atividade2
+{
+    public class Funcionario
+    {
+        public string Nome { get; set; }
+        public double Salario { get; set; }
+        public double Imposto { get; set; }
+
+        public double SalarioLiquido()
+        {
+            return Salario - Imposto;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            double diferenca = Salario * (porcentagem / 100);
+            double salarioLiquido = SalarioLiquido();
+            salarioLiquido += diferenca;
+            Console.WriteLine($"Dados Atualizados: {Nome}, {salarioLiquido}.");
+        }
+
+        public Funcionario(string nome, double salario, double imposto)
+        {
+            this.Salario = salario;
+            this.Imposto = imposto; 
+            this.Nome = nome;
+        }
+    }
+}
---
+++ Atividades/Classes, métodos/Atividade 2/Atividade2/Atividade2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Atividade2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Nome:");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Salário Bruto:");
+            double salarioBruto = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Imposto:");
+            double imposto = double.Parse(Console.ReadLine());
+
+            Funcionario funcionario = new Funcionario(nome, salarioBruto, imposto);
+
+            Console.WriteLine($"Funcionário {nome}, ${funcionario.SalarioLiquido()}.");
+
+            Console.WriteLine("Digite a porcentagem para aumentar o salário: ");
+            double porcentagem = double.Parse(Console.ReadLine());  
+
+            funcionario.AumentarSalario( porcentagem );
+        }
+    }
+}
